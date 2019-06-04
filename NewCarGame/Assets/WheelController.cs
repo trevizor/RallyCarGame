63d0d927f320fcc2fc90523036831c2f0f18bdc0
Glauber 'trevis' Bonifacio <glauber.bonifacio@gmail.com>
2019-06-04 19:21:19 -0300

    new controls, stuff new models

---
+++ NewCarGame/Assets/WheelController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WheelController : MonoBehaviour
+{
+    private float lastRotation;
+    private bool isDirectionRight = false;
+    private int turnCountRight = 0;
+    private int turnCountLeft = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.Log(this.gameObject.transform.rotation.eulerAngles);
+    }
+}
